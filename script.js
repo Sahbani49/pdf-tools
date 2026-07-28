6d3b57da1a1d9490b8d4725a38af25edfc666168
Sahbani49 <elmostafakaoui@gmail.com>
2026-07-28 22:31:28 +0100

    Remove touchmove event listener
    Removed touchmove event listener to improve performance.

--- script.js
+++ script.js
@@ -163,11 +163,7 @@ document.addEventListener("touchstart", function(e){
 });
 
 
-document.addEventListener("touchmove", function(e){
 
-    e.preventDefault();
-
-}, {passive:false});
 
 
 document.addEventListener("touchend", function(e){
